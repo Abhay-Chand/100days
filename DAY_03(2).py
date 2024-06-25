65ab7afef24c29d609a71e6698f37dfd1f452bd5
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2024-06-25 18:22:31 +0000

    if-else

---
+++ DAY_03(2).py
@@ -0,0 +1,20 @@
+#else Condition
+#SYNTAX:
+'''if [boolean expression]:
+    statement1
+    statement2
+    ...
+    statementN
+else:
+    statement1
+    statement2
+    ...
+    statementN'''
+#QUESTION:
+price = 50
+
+if price >= 100:
+    print("price is greater than 100")
+else:
+    print("price is less than 100")
+
