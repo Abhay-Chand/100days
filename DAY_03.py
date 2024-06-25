665d7edae59cada1f62e62c28fb3e5125ae678b5
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2024-06-25 18:19:30 +0000

    conditional loop (if )

---
+++ DAY_03.py
@@ -0,0 +1,23 @@
+# CONDITIONAL LOOP
+
+#SYNTAX
+'''if [boolean expression]:
+    statement1
+    statement2
+    ...
+    statementN  '''
+
+#QUESTION:
+price =int(input("Enter your price :"))
+
+if price < 100:
+    print("price is less than 100")
+
+# QUESTION  : MULTIPLE IF STATEMENT
+quantity = 5
+
+if price*quantity < 500:
+    print("price*quantity is less than 500")
+    print("price = ", price)
+    print("quantity = ", quantity)
+
