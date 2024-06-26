bc4b28d49ffb269d2fa8ccfce0c53c56cb5f7ed8
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2024-06-26 03:25:01 +0000

    if-elif-else condition

---
+++ DAY_04.py
@@ -0,0 +1,31 @@
+# elif Condition
+# syntax
+'''if [boolean expression]:
+    [statements]
+elif [boolean expresion]:
+    [statements]
+elif [boolean expresion]:
+    [statements]
+else:
+    [statements]   '''
+
+# QUESTION:
+price = int(input("enter the price"))
+
+if price > 100:
+  print("price is greater than 100 :")
+elif price == 100:
+  print("price is 100")
+elif price < 100:
+  print("price is less than 100")
+
+#here we use if-elif-else condition
+#QUESTION -02:
+prize = 50
+
+if prize > 100:
+  print("prize is greater than 100")
+elif prize == 100:
+  print("priez is 100")
+else:
+  print("prize is less than 100")
