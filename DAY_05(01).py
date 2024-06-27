49d8cf542b3cc123eeab25c8f0d63518aaa5fa2c
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2024-06-27 15:16:27 +0000

    performing different operations

---
+++ DAY_05(01).py
@@ -0,0 +1,40 @@
+#Indexing
+my_list = [10, 20, 'Jessa', 12.50, 'Emma']
+# accessing 2nd element of the list
+print(my_list[1]
+# accessing 5th element of the list
+print(my_list[4])
+
+
+#Negative Indexing
+my_list = [10, 20, 'Jessa', 12.50, 'Emma']
+      # accessing last element of the list
+print(my_list[-1])
+
+
+      # accessing second last element of the list
+print(my_list[-2])
+    
+
+      # accessing 4th element from last
+print(my_list[-4])
+
+
+#List Slicing
+my_list = [10, 20, 'Jessa', 12.50, 'Emma', 25, 50]
+# Extracting a portion
+
+#Iterating a List
+my_list = [5, 8, 'Tom', 7.50, 'Emma']
+
+# iterate a list
+for item in my_list:
+    print(item)
+
+#Iterate along with an index number
+my_list = [5, 8, 'Tom', 7.50, 'Emma']
+
+# iterate a list
+for i in range(0, len(my_list)):
+    # print each item using index number
+    print(my_list[i])
