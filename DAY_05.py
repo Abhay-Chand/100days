743a52f0c34ddddc2a2297945e9a0530d35e2a10
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2024-06-27 15:08:54 +0000

    list

---
+++ DAY_05.py
@@ -0,0 +1,26 @@
+# LIST 
+# SYNTAX:
+'''
+names = ["Jack", "Jill", "Harvey", "Bill", "Joseph"]
+'''
+
+# Using list constructor
+my_list1 = list((1, 2, 3))
+print(my_list1)
+
+
+# Using square brackets[]
+my_list2 = [1, 2, 3]
+print(my_list2)
+
+# with heterogeneous items
+my_list3 = [1.0, 'Jessa', 3]
+print(my_list3)
+
+# empty list using list()
+my_list4 = list()
+print(my_list4)
+
+# empty list using []
+my_list5 = []
+print(my_list4)
