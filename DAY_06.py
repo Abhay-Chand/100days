4d78edb316189dfa78b28fe7981401b721adc490
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2024-06-29 18:05:38 +0000

    FUNCTIONS

---
+++ DAY_06.py
@@ -0,0 +1,11 @@
+#  FUNCTIONS
+'''A function is a block of code that performs a specific task.
+A function can take some input, known as parameters, and return some output, known as the result.
+A function can be reused multiple times, which makes the code more modular and efficient.
+'''
+# Define the function
+def greet():
+print("Hello World!")
+
+# Call the function
+greet()
